cd756394199e32dd9cfec4ed048681dc1e81b1a4
Roj Vroemen <me@rojvroemen.com>
2014-09-19 14:48:13 +0200

    Updating migration file

--- server-php/database/migrations/2014_09_18_233512_create_flipper_maps_tables.php
+++ server-php/database/migrations/2014_09_18_233512_create_flipper_maps_tables.php
@@ -1,10 +1,9 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFlipperMapsTables extends Migration {
-
+class CreateFlipperMapsTables extends Migration
+{
 	/**
 	 * Run the migrations.
 	 *
@@ -12,33 +11,65 @@ class CreateFlipperMapsTables extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('countries', function($table)
-		{
+		Schema::create('countries', function ($table) {
 			$table->increments('id');
+			$table->string('name');
+			$table->string('code');
 			$table->timestamps();
 		});
 
-		Schema::create('cities', function($table)
-		{
+		Schema::create('cities', function ($table) {
 			$table->increments('id');
+			$table->integer('country_id')->unsigned();
+			$table->string('name');
 			$table->timestamps();
 		});
 
-		Schema::create('locations', function($table)
-		{
+		Schema::create('locations', function ($table) {
 			$table->increments('id');
+			$table->integer('city_id')->unsigned();
+			$table->string('name');
 			$table->timestamps();
 		});
 
-		Schema::create('machines', function($table)
-		{
+		Schema::create('models', function ($table) {
 			$table->increments('id');
+			$table->string('name');
+			$table->text('description');
 			$table->timestamps();
 		});
 
-		Schema::create('rates', function($table)
-		{
+		Schema::create('machines', function ($table) {
 			$table->increments('id');
+			$table->integer('location_id')->unsigned();
+			$table->integer('model_id')->unsigned();
+			$table->timestamps();
+		});
+
+		Schema::create('rates', function ($table) {
+			$table->increments('id');
+			$table->integer('machine_id')->unsigned();
+			$table->decimal('price');
+			$table->integer('games')->unsigned();
+			$table->timestamps();
+		});
+
+		Schema::create('notes', function ($table) {
+			$table->increments('id');
+			$table->integer('user_id')->unsigned();
+			$table->integer('noteable_id')->unsigned();
+			$table->string('noteable_type');
+			$table->string('type');
+			$table->text('note');
+			$table->timestamps();
+		});
+
+		Schema::create('ratings', function ($table) {
+			$table->increments('id');
+			$table->integer('rateable_id')->unsigned();
+			$table->string('rateable_type');
+			$table->integer('rating');
+			$table->integer('votes');
 			$table->timestamps();
 		});
 	}
@@ -50,8 +81,11 @@ class CreateFlipperMapsTables extends Migration {
 	 */
 	public function down()
 	{
+		Schema::drop('ratings');
+		Schema::drop('notes');
 		Schema::drop('rates');
 		Schema::drop('machines');
+		Schema::drop('models');
 		Schema::drop('locations');
 		Schema::drop('cities');
 		Schema::drop('countries');
