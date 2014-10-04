67087436197409e1356d760d7a2c500b55a8ec2d
Koen Schmeets <k.schmeets@gmail.com>
2014-10-04 08:48:41 +0200

    Adding debounce to dblistener (shitload faster =D)

--- server-js/dblistener.js
+++ server-js/dblistener.js
@@ -5,6 +5,8 @@ var pg = require ('pg')
   , _ = require('underscore');
 
 pg.connect('postgres://flippermaps:flippermaps@localhost:5432/flippermapsjs', function(err, client) {
+  var timers = {};
+
   if (err) {
     console.log(err);
   }
@@ -18,38 +20,47 @@ pg.connect('postgres://flippermaps:flippermaps@localhost:5432/flippermapsjs', fu
       , table = parts[1]
       , id = parts[2];
 
-    console.log(type, table, id);
+    var timerId = type + table + id;
 
-    if (type === 'change') {
-      if (table.indexOf('_') > -1) {
-        // relation table
-        app.models[table].find({
-          where: {
-            id: id
-          }
-        }).then(function(item) {
-          _.each(item, function(value, key) {
-            if (key.indexOf('_id') > -1) {
-              contentTypeKey = key.replace('_id', '');
-              contentType = app.contentTypes.get(contentType);
-              app.storageService.find(contentType, value).then(function(item) {
-                app.elasticsearchService.index(contentType, item);
-              });
+    // debounce it to speed it up
+    if (timers[timerId]) {
+      clearTimeout(timers[timerId]);
+    }
+
+    timers[type + table + id] = setTimeout(function() {
+      console.log(timerId);
+
+      if (type === 'change') {
+        if (table.indexOf('_') > -1) {
+          // relation table
+          app.models[table].find({
+            where: {
+              id: id
             }
+          }).then(function(item) {
+            _.each(item, function(value, key) {
+              if (key.indexOf('_id') > -1) {
+                contentTypeKey = key.replace('_id', '');
+                contentType = app.contentTypes.get(contentType);
+                app.storageService.find(contentType, value).then(function(item) {
+                  app.elasticsearchService.index(contentType, item);
+                });
+              }
+            });
           });
-        });
-      } else {
-        // normal table
-        contentType = app.contentTypes.get(table);
-        app.storageService.find(contentType, id).then(function(item) {
-          app.elasticsearchService.index(contentType, item);
-        });
+        } else {
+          // normal table
+          contentType = app.contentTypes.get(table);
+          app.storageService.find(contentType, id).then(function(item) {
+            app.elasticsearchService.index(contentType, item);
+          });
+        }
       }
-    }
 
-    if (type === 'destroy') {
-      // @todo implement
-    }
+      if (type === 'destroy') {
+        // @todo implement
+      }
+    }, 3000);
   });
 
   client.query('LISTEN watchers');
